cd9bcf4c64123878cb314e27f0bb97617d886d6c
Devdatta Thorat <141704064+devdatta1429@users.noreply.github.com>
2026-03-24 16:51:52 +0530

    Update Count.java

--- Java/Day 04 24-03/Count.java
+++ Java/Day 04 24-03/Count.java
@@ -1,4 +1,8 @@
-package demo;
+// You have to write a program to count the number of words in the input. 
+// The program should read in the input text till end_of_file (EOF) and output the number of words found.
+// A word can be taken to be a sequence of alphanumeric characters terminated by a space or by a newline.
+// Assume that there will not be any characters other than alphanumeric (a-z, A-Z, 0-9) and white spaces (blank, tabs and newlines) in the  input.
+
 import java.util.Scanner;
 
 public class Count{
