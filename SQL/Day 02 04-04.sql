31af43d2808555d9882664935b00ca1827db3479
Devdatta Thorat <141704064+devdatta1429@users.noreply.github.com>
2026-04-04 15:09:59 +0530

    Update Day 02 04-04.sql

--- SQL/Day 02 04-04.sql
+++ SQL/Day 02 04-04.sql
@@ -84,3 +84,61 @@ age int generated always as (datediff(today,dob)) virtual
 insert into to_virtual1(f_name,l_name,dob) values("Devdatta","Thorat","2003-06-29");
 select * from to_virtual1;
 
+
+-- ======================================lunch uploaad========================================
+
+create table to_virtual2(
+f_name varchar(30),
+l_name varchar(30),
+dob date,
+doj date,
+-- today date default (curdate()),
+full_name varchar(100) generated always as (concat(f_name," ",l_name)) virtual,
+age int generated always as (datediff(doj,dob)/365) virtual,
+salary int,
+bonus int,
+increment decimal(4,2),
+new_salary int generated always as (salary+((salary+bonus)*increment)) virtual
+);
+
+-- ============================ json ==================================
+
+create table to_virtual_json(
+f_name varchar(30),
+l_name varchar(30),
+dob date,
+doj date,
+-- today date default (curdate()),
+full_name varchar(100) generated always as (concat(f_name," ",l_name)) virtual,
+age int generated always as (datediff(doj,dob)/365) virtual,
+salary int,
+bonus int,
+increment decimal(4,2),
+new_salary int generated always as (salary+((salary+bonus)*increment)) virtual,
+skills json
+);
+
+insert into to_virtual2 (f_name,l_name,dob,doj,salary,bonus,increment) 
+values("devdatta", "thorat" , "2003-06-29" ,"2026-02-26" ,25000,2000,15);
+
+select * from to_virtual2;
+insert into to_virtual_json (f_name,l_name,skills) 
+values("dev","thorat",'{"skills":["python","sql"],"certifcation":["hackerrank","leetcode"],"edu":"cdac"}');
+
+select * from to_virtual_json;
+
+select full_name, skills ->> '$.skills[0]' as skill, skills ->> '$.edu' as education from to_virtual_json;
+
+-- ===========================================================================================
+
+create table json_new(
+f_name varchar(10),
+l_name varchar(10),
+skills_set json
+);
+
+insert into json_new (f_name,l_name,skills_set) values ("arv","sonakr",'{"tv":["LG","Sony"],"laptop":["lenovo","Asus"]}');
+
+select f_name,skills_set ->> '$.tv[1]' as tv ,skills_set ->> '$.laptop[0]' as luptup from json_new;
+
+
