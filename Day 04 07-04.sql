8f82397899bfed9e38851fe4f31260267afb48fe
Devdatta Thorat <141704064+devdatta1429@users.noreply.github.com>
2026-04-07 10:49:36 +0530

    Add files via upload

---
+++ Day 04 07-04.sql
@@ -0,0 +1,109 @@
+use db;
+
+create table dept_using_cud_fk_ex(
+	dept_id int auto_increment primary key,
+    dept_name varchar(50),
+    emp_id int,
+    project_id varchar(20)
+);
+
+create table emp_using_cud_fk_ex(
+	emp_id int auto_increment primary key,
+    emp_name varchar(30),
+    p_id varchar(30),
+    department_id int,
+    foreign key (department_id) references dept_using_cud_fk_ex(dept_id) 
+    on update cascade
+    on delete cascade
+);
+
+insert into dept_using_cud_fk_ex(dept_name,emp_id ,project_id) value('IT',103,'P_01'),
+('Admin',104,'P_02'),('HR',105,'P_011');
+
+insert into emp_using_cud_fk_ex(emp_name,p_id ,department_id)
+values
+('A','P_02',3),
+('B','P_02',1),
+('AA','P_03',1),
+('BB','P_03',3);
+
+select * from dept_using_cud_fk_ex;
+select * from emp_using_cud_fk_ex;
+
+update emp_using_cud_fk_ex set department_id = 5 where department_id =1;
+# 10:15:40	update emp_using_cud_fk_ex set department_id = 5 where department_id =1	
+# Error Code: 1452. Cannot add or update a child row: a foreign key constraint fails (`db`.`emp_using_cud_fk_ex`, CONSTRAINT `emp_using_cud_fk_ex_ibfk_1` 
+# FOREIGN KEY (`department_id`) REFERENCES `dept_using_cud_fk_ex` (`dept_id`) ON DELETE CASCADE ON UPDATE CASCADE)	0.000 sec
+
+update dept_using_cud_fk_ex set dept_id = 13 where dept_id = 3;
+
+## ====================  Set Null  ====================================================================================
+## Set null
+create table dept_using_sn_fk_ex(
+	dept_id int auto_increment primary key,
+    dept_name varchar(50),
+    emp_id int,
+    project_id varchar(20)
+);
+
+create table emp_using_sn_fk_ex(
+	emp_id int auto_increment primary key,
+    emp_name varchar(30),
+    p_id varchar(30),
+    department_id int,
+    foreign key (department_id) references dept_using_sn_fk_ex(dept_id) on delete set null
+);
+
+insert into dept_using_sn_fk_ex(dept_name,emp_id ,project_id) value('IT',103,'P_01'),
+('Admin',104,'P_02'),('HR',105,'P_011');
+
+insert into emp_using_sn_fk_ex(emp_name,p_id ,department_id)
+values
+('A','P_02',3),
+('B','P_02',1),
+('AA','P_03',1),
+('BB','P_03',3);
+
+select * from dept_using_sn_fk_ex; #### parent table = department
+select * from emp_using_sn_fk_ex;
+
+delete from dept_using_sn_fk_ex where dept_id =3;
+
+
+## ====================  Restrict  ====================================================================================
+## Restrict
+create table dept_using_r_fk_ex(
+	dept_id int auto_increment primary key,
+    dept_name varchar(50),
+    emp_id int,
+    project_id varchar(20)
+);
+
+create table emp_using_r_fk_ex(
+	emp_id int auto_increment primary key,
+    emp_name varchar(30),
+    p_id varchar(30),
+    department_id int,
+    foreign key (department_id) references dept_using_r_fk_ex(dept_id) on delete restrict
+);
+
+insert into dept_using_r_fk_ex(dept_name,emp_id ,project_id) value('IT',103,'P_01'),
+('Admin',104,'P_02'),('HR',105,'P_011');
+
+insert into emp_using_r_fk_ex(emp_name,p_id ,department_id)
+values
+('A','P_02',3),
+('B','P_02',1),
+('AA','P_03',1),
+('BB','P_03',3);
+
+select * from dept_using_r_fk_ex; #### parent table = department
+select * from emp_using_r_fk_ex;
+
+delete from dept_using_r_fk_ex where dept_id =3;
+# 10:32:25	delete from dept_using_r_fk_ex where dept_id =3	
+# Error Code: 1451. Cannot delete or update a parent row: a foreign key constraint fails (`db`.`emp_using_r_fk_ex`, CONSTRAINT `emp_using_r_fk_ex_ibfk_1` 
+# FOREIGN KEY (`department_id`) REFERENCES `dept_using_r_fk_ex` (`dept_id`) ON DELETE RESTRICT)	0.000 sec
+
+# ==============================================================================
+
